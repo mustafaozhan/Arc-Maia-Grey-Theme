db0571b7a5e03d8e1fbba15d3265a0cb3c403893
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2018-01-15 14:21:21 +0300

    installation scprit updated

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo mv * /usr/share/themes/
+sudo rm -rf /usr/share/themes/Arc-Maia-Grey /usr/share/themes/Arc-Maia-Grey-Dark /usr/share/themes/Arc-Maia-Grey-Darker;
+sudo mv * /usr/share/themes/;
 cd ..;
-rm -rf Arc-Maia-Grey;
+rm -rf Arc-Maia-Grey-Theme;
